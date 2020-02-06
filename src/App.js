2104fff74f2e925edd3ade51d308579785bb880b
Fuadit Muhammad <muhammadfuaditrockz@gmail.com>
2020-02-06 11:42:44 +0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Catch Coding Challenge
         </a>
       </header>
     </div>
