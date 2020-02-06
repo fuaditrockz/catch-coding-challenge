5c59f2c68baa870f52901dda43c17ae08af01b66
Fuadit Muhammad <muhammadfuaditrockz@gmail.com>
2020-02-06 13:18:04 +0700

    Make responsive product card

---
+++ src/components/ProductCard.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { Card, Col } from 'antd';
+
+export default class ProductCard extends Component {
+  render() {
+    return (
+      <Col xs={10} sm={10} md={8} lg={6} xl={4}>
+        <Card
+          hoverable
+          style={{ width: "100%" }}
+          cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+        >
+          <Card.Meta title="Europe Street beat" description="www.instagram.com" />
+        </Card>
+      </Col>
+    );
+  }
+}
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,47 +1,18 @@
 import React, { Component } from 'react';
 import { Layout, Row, Col, Card } from 'antd';
 
+import ProductCard from './ProductCard';
+
 export default class ProductList extends Component {
   render() {
     return (
       <Layout.Content style={containerStyle}>
-        <Row gutter={10}>
-          <Col className="gutter-row" span={6}>
-            <Card
-              hoverable
-              style={{ width: "100%" }}
-              cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
-            >
-              <Card.Meta title="Europe Street beat" description="www.instagram.com" />
-            </Card>
-          </Col>
-          <Col className="gutter-row" span={6}>
-            <Card
-              hoverable
-              style={{ width: "100%" }}
-              cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
-            >
-              <Card.Meta title="Europe Street beat" description="www.instagram.com" />
-            </Card>
-          </Col>
-          <Col className="gutter-row" span={6}>
-            <Card
-              hoverable
-              style={{ width: "100%" }}
-              cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
-            >
-              <Card.Meta title="Europe Street beat" description="www.instagram.com" />
-            </Card>
-          </Col>
-          <Col className="gutter-row" span={6}>
-            <Card
-              hoverable
-              style={{ width: "100%" }}
-              cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
-            >
-              <Card.Meta title="Europe Street beat" description="www.instagram.com" />
-            </Card>
-          </Col>
+        <Row gutter={[10, 10]}>
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
         </Row>
       </Layout.Content>
     )
