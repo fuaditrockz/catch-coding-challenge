d7987d3ea63fe05f8e2e9e2dda6634867bfaeaee
Fuadit Muhammad <muhammadfuaditrockz@gmail.com>
2020-02-06 23:01:14 +0700

    Setup redux

---
+++ src/reducers/cartProducts.js
@@ -0,0 +1,8 @@
+const cartProducts = (state = [], action) => {
+  switch (action.type) {
+    default:
+      return null
+  }
+}
+
+export default cartProducts;
---
+++ src/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux';
+import cartProducts from './cartProducts';
+
+export default combineReducers({
+  cartProducts,
+})
