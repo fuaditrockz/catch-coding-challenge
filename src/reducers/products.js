8c100d8a7258928bbe770d59d7363704b71b83b2
Fuadit Muhammad <muhammadfuaditrockz@gmail.com>
2020-02-15 18:19:37 +0700

    change reducers for products.

--- src/reducers/products.js
+++ src/reducers/products.js
@@ -1,39 +1,58 @@
 import {
-  FETCH_PENDING,
-  FETCH_SUCCESS,
-  FETCH_ERROR
-} from '../constants/actionTypes'
+  PENDING,
+  SET_ITEMS_SUCCESS,
+  SET_ITEMS_ERROR,
+  SET_CART_SUCCESS,
+  SET_CART_ERROR
+} from "../constants/actionTypes"
 
 const initialState = {
   items: [],
+  cart: { list: [], total: 0 },
   loading: false,
   error: null
 }
 
 function productsReducer(state = initialState, action) {
   switch (action.type) {
-    case FETCH_PENDING:
+    case PENDING:
       return {
         ...state,
         loading: true,
         error: null
       }
-    case FETCH_SUCCESS:
+    case SET_ITEMS_SUCCESS:
       return {
         ...state,
         loading: false,
         items: action.payload.products
       }
-    case FETCH_ERROR:
+    case SET_ITEMS_ERROR:
       return {
         ...state,
         loading: false,
-        error: action.payload,
+        error: action.payload.error,
         items: []
       }
+    case SET_CART_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        cart: { list: action.payload.cart, total: action.payload.total }
+      }
+    case SET_CART_ERROR:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload.error,
+        cart: {
+          list: [],
+          total: 0
+        }
+      }
     default:
-      return state;
+      return state
   }
 }
 
-export default productsReducer;
+export default productsReducer
